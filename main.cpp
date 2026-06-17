7d772f6fa96fd129f3a75efab90c847ba04303c4
AxelLima08 <axellima726@gmail.com>
2026-06-17 20:38:50 -0300

    release: 0.2.0
    Se agrego las funciones iniciarWiFiYNTP y actualizarTiempoReal

--- main.cpp
+++ main.cpp
@@ -1,53 +1,72 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+
 #define sensorHallFinal 14
 #define sensorHallInicio 13
 #define motorDCsentidoHorario 16 //Cambiar segun el pin que se use para controlar el motor DC en sentido horario
 #define motorDCsentidoAntiHorario 17 //Cambiar segun el pin que se use para controlar el motor DC en sentido antihorario
-//Se tomara como true para un estado alto del pin y false para un estado bajo del pin
 bool calibracionCompleta = false;
+const char* SSID = "TuSSID"; // Cambia esto por el nombre de tu red WiFi
+const char* PASSWORD = "TuPassword"; // Cambia esto por la contraseña de tu red WiFi
+bool wifiYntp = false; // Variable para controlar si se ha iniciado WiFi y NTP correctamente
+struct tm tiempoReal;
+
+
+void actualizarTiempoReal(){
+  if(wifiYntp){
+    if(getLocalTime(&tiempoReal)){
+      // El tiempo se ha actualizado correctamente, puedes usar la variable tiempoReal para lo que necesites
+    }
+  }
+}
 
 
 void calibrarMotorDC(){
-  if(calibracionCompleta) return;  // Ya calibrado, salir. A esto se le llama Esrly Return, es una buena práctica para evitar anidar código innecesariamente y mejorar la legibilidad del código.
+  if(calibracionCompleta) return;  // Ya calibrado, salir. A esto se le llama Early Return, es una buena práctica para evitar anidar código innecesariamente y mejorar la legibilidad del código.
 
 
-  static int8_t estadocalibrarMotorDC = 1;//Static para que estadocalibrarMotorDC no se reasigne a 1 cada vez que se llame a la función calibrarMotorDC, sino que mantenga su valor entre llamadas a la función
-  switch(estadocalibrarMotorDC)
-  {
-
-
-  case 1://Estado buscando Final
-    if(!digitalRead(sensorHallFinal)){
-      estadocalibrarMotorDC = 2;
-    }
-    else{
-        digitalWrite(motorDCsentidoAntiHorario, HIGH);
-      }
-  break;
-
-
-  case 2://Esta en el final
+ if (!digitalRead(sensorHallFinal)) {
+    digitalWrite(motorDCsentidoAntiHorario, HIGH);
+} else {
     digitalWrite(motorDCsentidoAntiHorario, LOW);
     calibracionCompleta = true;
-  break;
+}
+}
 
 
-  default:
-    break;
+void iniciarWiFiYNTP(){
+  if(wifiYntp) return; // Ya iniciado, salir
+ static bool wifiIniciado = false;
+ if(!wifiIniciado){
+   WiFi.begin(SSID, PASSWORD);
+   wifiIniciado = true;
+ }
+  if(WiFi.status() == WL_CONNECTED){
+    configTime(-10800, 0, "pool.ntp.org");
+    wifiYntp = true;
+    actualizarTiempoReal();
+  }
+  else {
+  // Añadir un retraso de 1 segundo
   }
 }
 
 
 void setup() {
-pinMode(sensorHallFinal, INPUT);
-pinMode(sensorHallInicio, INPUT);
-pinMode(motorDCsentidoHorario, OUTPUT);
-pinMode(motorDCsentidoAntiHorario, OUTPUT);
+  pinMode(sensorHallFinal, INPUT);
+  pinMode(sensorHallInicio, INPUT);
+  pinMode(motorDCsentidoHorario, OUTPUT);
+  pinMode(motorDCsentidoAntiHorario, OUTPUT);
 }
 
 
 void loop() {
-calibrarMotorDC();
+  calibrarMotorDC();
+  iniciarWiFiYNTP();
 }
+
+
 
 
 
