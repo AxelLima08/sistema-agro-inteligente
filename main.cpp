4f700bb3f694a065c0944849fb9165f51b58cb44
AxelLima08 <axella726@gmail.com>
2026-09-15 08:53:05 -0300

    Migracion completa a FreeRTOS

--- main.cpp
+++ main.cpp
@@ -4,14 +4,27 @@
 #define seg10 10000
 #define bombaDeAgua 6
 #define sensorHumedad 7
+#define motorDCAbrir 16
+#define motorDCCerrar 17
+#define sensorHallCerrado 14
+#define sensorHallAbierto 13
 #define estaSeco 2800
 #define estaHumedo 1200
+#define sensorMovimiento 5
+#define sensorLluvia 4
+
 void tareaRegar(void *pvParameters);
-void tareaCalibrarMotor(void *pvParameters);
+void detectarLluvia(void *pvParameters);
 
 void setup() {
-  pinMode(sensorHumedad,INPUT);
+  pinMode(sensorHallCerrado, INPUT_PULLUP);
+  pinMode(sensorHallAbierto, INPUT_PULLUP);
+  pinMode(motorDCCerrar, OUTPUT);
+  pinMode(motorDCAbrir, OUTPUT);
+  pinMode(sensorMovimiento, INPUT);
+  pinMode(sensorHumedad, INPUT);
   pinMode(bombaDeAgua, OUTPUT);
+  pinMode(sensorLluvia, INPUT);
   xTaskCreate(
     tareaRegar,
     "Regar",
@@ -21,13 +34,13 @@ void setup() {
     NULL
   );
   xTaskCreate(
-    tareaCalibrarMotor,
-    "CalibrarMotorDC",
+    detectarLluvia,
+    "detectarLluvia",
     2048,
     NULL,
     1,
     NULL
-  )
+  );
 }
 
 
@@ -35,19 +48,6 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 
-}
-
-void tareaCalibrar(void *pvParameters){
-  while(1){
-    if (digitalRead(sensorHallFinal) == LOW) {
-      digitalWrite(motorDCsentidoAntiHorario, HIGH);
-    }
-    else {
-      digitalWrite(motorDCsentidoAntiHorario, LOW);
-      vTaskDelete(NULL);
-    }
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
 }
 
 void tareaRegar(void *pvParameters){
@@ -74,4 +74,37 @@ void tareaRegar(void *pvParameters){
       break;
     }
   }
+}
+
+void detectarLluvia(void *pvParameters){
+  uint8_t estadoLluvia = 0;
+  while(1){
+  switch(estadoLluvia){
+    case 0:
+    if(analogRead(sensorLluvia) < 500){
+      digitalWrite(motorDCAbrir, HIGH);
+      estadoLluvia = 1;
+    }
+    break;
+    case 1:
+    if(digitalRead(sensorHallAbierto) == HIGH){
+      digitalWrite(motorDCAbrir, LOW);
+      estadoLluvia = 2;
+    }
+    break;
+    case 2:
+    if(analogRead(sensorLluvia) > 2000){
+      digitalWrite(motorDCCerrar, HIGH);
+      estadoLluvia = 3;
+    }
+    break;
+    case 3:
+    if(digitalRead(sensorHallCerrado) == HIGH){
+      digitalWrite(motorDCCerrar, LOW);
+      estadoLluvia = 0;
+    }
+    break;
+  }
+  vTaskDelay(pdMS_TO_TICKS(10));
+}
 }
